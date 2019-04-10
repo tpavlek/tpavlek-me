6670346ba71072ba321806edf525ee6d489b03ee
Troy Pavlek <troy@tpavlek.me>
2019-04-09 18:28:06 -0600

    Unwrapped pages also have social meta

--- source/_layouts/unwrapped.blade.php
+++ source/_layouts/unwrapped.blade.php
@@ -1,5 +1,30 @@
 @extends('_layouts.master')
 
+@section('social_meta')
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:site" content="@troypavlek"/>
+
+    <meta name="twitter:title" content="{{ $page->title }}"/>
+    <meta property="og:title" content="{{ $page->title }}"/>
+
+    @if($page->description)
+        <meta property="og:description" content="{{ $page->description }}"/>
+    @else
+        <meta property="og:description" content="Troy Pavlek -- Edmonton City Council Candidate for Ward 11"/>
+    @endif
+
+    @if ($page->imgpath && $page->img)
+        <meta property="og:image" content="{{ $page->baseUrl }}{{$page->imgpath}}{{$page->img}}"/>
+    @else
+        <meta property="og:image" content="{{ $page->baseUrl }}/img/splash-banner-bg.jpg"/>
+    @endif
+
+
+    <meta property="og:type" content="article"/>
+    <meta property="fb:admins" content="551604144"/>
+    <meta property="fb:app_id" content="691298577729703"/>
+@stop
+
 @section('body_content')
     <div class="content-wrapper unwrapped">
         @yield('content')
