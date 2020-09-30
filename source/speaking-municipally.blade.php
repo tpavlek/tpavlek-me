7d31ef59e9a5b5eb25247cd492d4ea5acb7fd347
Troy Pavlek <troy@tpavlek.me>
2020-09-30 14:45:14 -0600

    New sm page

--- source/speaking-municipally.blade.php
+++ source/speaking-municipally.blade.php
@@ -11,24 +11,61 @@
     <div class="bg-purple-darkest text-white text-center p-8">
         <h1 class="text-4xl my-4">Curious about Edmonton municipal politics?</h1>
 
-        <p class="text-xl leading-loose max-w-xl text-center py-4 mx-auto">
-            <a href="https://twitter.com/mastermaq" class="text-white">Mack Male</a> and I have just the podcast for you!
-            <br />
-            Each week on Speaking Municipally, we'll break down the events of the past week, give you our take on them
-            and give you the context of how these decisions got made and why they matter to you.
-            <br />
-            All in under thirty minutes, without (we hope) being boring for any of those minutes.
-        </p>
+        <div class="lg:flex">
+            <p class="text-xl leading-loose max-w-xl lg:text-left py-4 mr-4 lg:w-1/2">
+                Each week, Speaking Municipally breaks down the events of the past week, gives you an informed take on them
+                dives into the context of how these decisions got made and why they matter to you.
+                <br />
+                All in under thirty minutes, without (we hope) being boring for any of those minutes.
+            </p>
 
-        <h1 class="text-2xl my-4">Latest Episode</h1>
+            <div class="lg:w-1/2 ml-4 flex flex-col items-center justify-center">
+                <iframe src='https://share.transistor.fm/e/speaking-municipally/latest' width='100%' height='180' frameborder='0' scrolling='no' seamless='true'></iframe>
+            </div>
+        </div>
 
-        <iframe src='https://share.transistor.fm/e/speaking-municipally/latest' width='100%' height='180' frameborder='0' scrolling='no' seamless='true'></iframe>
 
     </div>
 
     <div class="text-white text-center p-8">
-        <h1 class="text-3xl my-4 ">Podcast Trailer</h1>
-        @include('_partials.youtube', [ 'video_id' => "RAyFYWOlFOs"])
+        <h1 class="text-3xl my-4 ">Who we are</h1>
+
+        <div class="lg:flex">
+
+            <div class="flex bg-white rounded m-8 shadow-md">
+                <div class="w-1/3 flex-none bg-cover bg-center overflow-hidden relative" style="background-image:url('/img/troy-profile.jpg');">
+                    <div class="w-12 absolute bg-white pin-r" style="margin-right: -3rem; height: 150%; transform-origin: center top; transform: rotate(9deg);">
+
+                    </div>
+                </div>
+                <div class="p-4 text-left text-grey-darkest">
+                    <h1 class="text-2xl m-0">Troy Pavlek</h1>
+                    <small class="italic">Co-host, editor, issuer of hot takes</small>
+                    <p class="mt-2 leading-loose">
+                        Troy's take on local politics comes from the unique cross-section of his interests in bikes, policy,
+                        internet culture and a rabid and undying love of The West Wing. He <a href="/blog/2017/10/17/i-lost-the-election">tried to run for city council in 2017</a>
+                        and failed very, very badly. Now he has a podcast, so take that haters, I guess.
+                    </p>
+                </div>
+            </div>
+
+            <div class="flex bg-white rounded m-8 shadow-md">
+                <div class="w-1/3 flex-none bg-cover bg-center overflow-hidden relative" style="background-image:url('/img/mack-profile.jpg');">
+                    <div class="w-12 absolute bg-white pin-r" style="margin-right: -3rem; height: 150%; transform-origin: center top; transform: rotate(9deg);">
+
+                    </div>
+                </div>
+                <div class="p-4 text-left text-grey-darkest">
+                    <h1 class="text-2xl m-0">Mack Male</h1>
+                    <small class="italic">Co-host, Business Daddy™, does all the actual work</small>
+                    <p class="mt-2 leading-loose">
+                        Mack is the co-founder of Taproot Publishing, the organization that backs Speaking Municipally each week.
+                        He's compiled "Edmonton Notes" on his blog for longer than humans have been keeping historical records which
+                        gives the podcast a unique flavour of having both hot takes and <span class="italic">facts</span>.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 
     <div class="bg-white text-center p-8">
