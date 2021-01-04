12f65865c5659b909f81f441c18aedcfbe54df1f
Troy Pavlek <troy@tpavlek.me>
2021-01-03 17:07:20 -0700

    typo

--- source/_partials/profile-card.blade.php
+++ source/_partials/profile-card.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col md:flex-row bg-white rounded my-2 md:m-8 shadow-md lg:w-1/2">
     <div class="w-full h-40 md:h-full md:w-1/3 flex-none bg-cover bg-top md:bg-center overflow-hidden relative" style="background-image:url('{{ $img }}');">
-        <div class="hidden md:block kw-12 absolute bg-white right-0" style="margin-right: -3rem; height: 150%; transform-origin: center top; transform: rotate(9deg);">
+        <div class="hidden md:block w-12 absolute bg-white right-0" style="margin-right: -3rem; height: 150%; transform-origin: center top; transform: rotate(9deg);">
         </div>
     </div>
     <div class="p-4 text-left text-grey-900">
